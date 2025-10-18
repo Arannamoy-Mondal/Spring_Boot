d304a397da6bbe1737e3643cd4324f84bc81dc5d
Arannamoy-Mondal <arannamoymondal13@gmail.com>
2025-10-19 02:43:22 +0600

    Test

---
+++ Main/src/test/java/Hibernate/Main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package Hibernate.Main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
