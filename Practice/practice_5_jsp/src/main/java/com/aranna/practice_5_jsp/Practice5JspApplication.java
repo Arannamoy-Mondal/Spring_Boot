3324a94af24eb29086624c6ccf78593820a78fc7
Arannamoy-Mondal <arannamoymondal13@gmail.com>
2025-10-30 19:06:54 +0000

    upd

---
+++ Practice/practice_5_jsp/src/main/java/com/aranna/practice_5_jsp/Practice5JspApplication.java
@@ -0,0 +1,13 @@
+package com.aranna.practice_5_jsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practice5JspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practice5JspApplication.class, args);
+	}
+
+}
---
+++ Practice/practice_5_jsp/src/test/java/com/aranna/practice_5_jsp/Practice5JspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aranna.practice_5_jsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practice5JspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
