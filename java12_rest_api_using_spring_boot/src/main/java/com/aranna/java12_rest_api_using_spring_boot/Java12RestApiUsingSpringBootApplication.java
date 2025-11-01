5becbbda09c5147f6cfcdbe8e0ebab1fb32ae7a0
Arannamoy-Mondal <arannamoymondal13@gmail.com>
2025-11-01 00:19:18 +0000

    add:java12_rest_api_using_spring_boot

---
+++ java12_rest_api_using_spring_boot/src/main/java/com/aranna/java12_rest_api_using_spring_boot/Java12RestApiUsingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.aranna.java12_rest_api_using_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java12RestApiUsingSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java12RestApiUsingSpringBootApplication.class, args);
+	}
+
+}
---
+++ java12_rest_api_using_spring_boot/src/test/java/com/aranna/java12_rest_api_using_spring_boot/Java12RestApiUsingSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aranna.java12_rest_api_using_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java12RestApiUsingSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
