83c12038590807979659c259a300b69d2e62ad95
Arannamoy Mondal <arannamoymondal13@gmail.com>
2026-02-28 23:25:25 +0600

    upd:Readme.md

---
+++ Testing/test_3_spring_grpc/src/main/java/com/test_3_spring_grpc/test_3_spring_grpc/Test3SpringGrpcApplication.java
@@ -0,0 +1,13 @@
+package com.test_3_spring_grpc.test_3_spring_grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test3SpringGrpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test3SpringGrpcApplication.class, args);
+	}
+
+}
---
+++ Testing/test_3_spring_grpc/src/test/java/com/test_3_spring_grpc/test_3_spring_grpc/Test3SpringGrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test_3_spring_grpc.test_3_spring_grpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test3SpringGrpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
