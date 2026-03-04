70397c5bc30fa709cd6c5b1da8933b9b98e9dc60
Arannamoy Mondal <arannamoymondal13@gmail.com>
2026-03-05 01:51:55 +0600

    upd:java25_mockito

---
+++ java26_soap_web_services/src/main/java/com/aranna/java26_soap_web_services/Java26SoapWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.aranna.java26_soap_web_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java26SoapWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java26SoapWebServicesApplication.class, args);
+	}
+
+}
---
+++ java26_soap_web_services/src/test/java/com/aranna/java26_soap_web_services/Java26SoapWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aranna.java26_soap_web_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java26SoapWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
