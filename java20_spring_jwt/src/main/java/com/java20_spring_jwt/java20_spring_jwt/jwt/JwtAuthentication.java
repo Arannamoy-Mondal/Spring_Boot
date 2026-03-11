c4f811ac3567f174ac2833e31f2b80679e39786c
Arannamoy Mondal <arannamoymondal13@gmail.com>
2026-03-11 15:13:30 +0600

    upd

--- java20_spring_jwt/src/main/java/com/java20_spring_jwt/java20_spring_jwt/jwt/JwtAuthentication.java
+++ java20_spring_jwt/src/main/java/com/java20_spring_jwt/java20_spring_jwt/jwt/JwtAuthentication.java
@@ -2,15 +2,23 @@ package com.java20_spring_jwt.java20_spring_jwt.jwt;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
 @RestController
 public class JwtAuthentication {
+
+    private JwtEncoder jwtEncoder;
+    public JwtAuthentication(JwtEncoder jwtEncoder){
+        this.jwtEncoder=jwtEncoder;
+    }
     @PostMapping("/authenticate")
     public Map<String, Object> authenticate(Authentication authentication) {   
         if (authentication == null) {
@@ -22,5 +30,31 @@ public class JwtAuthentication {
         response.put("isAuthenticated", authentication.isAuthenticated());
         return response;
     }
+
+
+    @PostMapping("/authenticate/v1")
+    public JwtResponse authenticateV1(Authentication authentication) {   
+      return new JwtResponse(createToken(authentication));
+    }
+
+    private String createToken(Authentication authentication){
+        JwtClaimsSet.builder().issuer("self")
+        .issuedAt(Instant.now())
+        .expiresAt(Instant.now().plusSeconds(60*24*60))
+        .subject(authentication.getName())
+        .claim("scope",createScope(authentication))
+        .build();
+        return 
+    }
+    private Object createScope(Authentication authentication) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createScope'");
+    }
     
+}
+
+
+record JwtResponse(String token) {
+
+
 }
