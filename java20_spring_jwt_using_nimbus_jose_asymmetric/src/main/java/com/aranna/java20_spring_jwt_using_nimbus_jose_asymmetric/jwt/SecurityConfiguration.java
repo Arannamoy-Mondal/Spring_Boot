7f27624a6672d994805a416ae76bf2d8bb2a5d4a
Arannamoy Mondal <arannamoymondal13@gmail.com>
2026-03-15 23:55:25 +0600

    upd:java20_spring_jwt_using_nimbus_jose_asymmetric

--- java20_spring_jwt_using_nimbus_jose_asymmetric/src/main/java/com/aranna/java20_spring_jwt_using_nimbus_jose_asymmetric/jwt/SecurityConfiguration.java
+++ java20_spring_jwt_using_nimbus_jose_asymmetric/src/main/java/com/aranna/java20_spring_jwt_using_nimbus_jose_asymmetric/jwt/SecurityConfiguration.java
@@ -5,15 +5,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
+
+    // securityFilterchain
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         return httpSecurity.authorizeHttpRequests(
             customizer->
             customizer
+            // .requestMatchers("/**")
+            // .permitAll()
             .anyRequest()
             .authenticated()
         )
@@ -22,9 +29,18 @@ public class SecurityConfiguration {
         // .csrf(Customizer.withDefaults())
         .httpBasic(Customizer.withDefaults())
         .formLogin(customizer->customizer.disable())
+        .headers(customizer->customizer.frameOptions().sameOrigin())
         .build();
     }
 
-
+    @Bean
+    public UserDetailsService userDetailsService(){
+        var user=User.builder().username("user")
+        .password("{noop}password")
+        // .passwordEncoder(null)
+        .roles("USER")
+        .build();
+        return new InMemoryUserDetailsManager(user);
+    }
 
 }
